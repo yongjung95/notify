8bea01f82ea258739d274416f8b3e450a63521e4
yongjung95 <yongjung95@gmail.com>
2022-10-11 17:14:39 +0900

    :recycle: KeywordRepository Querydsl 적용

--- src/main/java/com/jung/notify/repository/KeywordRepository.java
+++ src/main/java/com/jung/notify/repository/KeywordRepository.java
@@ -2,27 +2,42 @@ package com.jung.notify.repository;
 
 import com.jung.notify.domain.Keyword;
 import com.jung.notify.domain.Member;
-import lombok.RequiredArgsConstructor;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
+import static com.jung.notify.domain.QKeyword.keyword1;
+import static com.jung.notify.domain.QMember.member;
+
 @Repository
-@RequiredArgsConstructor
 public class KeywordRepository {
 
     private final EntityManager em;
+
+    private final JPAQueryFactory queryFactory;
+
+    public KeywordRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
 
     public void save(Keyword keyword){
         em.persist(keyword);
     }
 
-    public Optional<Keyword> findOne(Long id, Member member){
-        List<Keyword> keywords = em.createQuery("select k from Keyword k where k.id = :id and k.member = :member", Keyword.class).setParameter("id", id).setParameter("member", member).getResultList();
+    public Optional<Keyword> findOne(Long id, Member searchMember){
+//        List<Keyword> keywords = em.createQuery("select k from Keyword k where k.id = :id and k.member = :member", Keyword.class).setParameter("id", id).setParameter("member", member).getResultList();
 
-        return keywords.stream().findAny();
+        return Optional.ofNullable(queryFactory
+                .select(keyword1)
+                .from(keyword1)
+                .leftJoin(keyword1.member, member)
+                .where(keyword1.id.eq(id)
+                        .and(member.eq(searchMember))
+                ).fetchOne());
     }
 
     public Optional<Keyword> findOneByKeyword(String keyword, Member member){
@@ -35,11 +50,23 @@ public class KeywordRepository {
     }
 
     public List<Keyword> findAll(){
-        return em.createQuery("select k from Keyword k join fetch k.member", Keyword.class).getResultList();
+//        return em.createQuery("select k from Keyword k join fetch k.member", Keyword.class).getResultList();
+
+        return queryFactory
+                .selectFrom(keyword1)
+                .leftJoin(keyword1.member, member)
+                .fetch();
     }
 
-    public List<Keyword> findAllByMember(Member member){
-        return em.createQuery("select k from Keyword k join fetch k.member where k.member = :member", Keyword.class).setParameter("member", member).getResultList();
+    public List<Keyword> findAllByMember(Member searchMember){
+//        return em.createQuery("select k from Keyword k join fetch k.member where k.member = :member", Keyword.class).setParameter("member", member).getResultList();
+
+        return queryFactory
+                .select(keyword1)
+                .from(keyword1)
+                .leftJoin(keyword1.member, member)
+                .where(member.eq(searchMember))
+                .fetch();
     }
 
     public void delete(Keyword keyword){
---
+++ src/test/java/com/jung/notify/repository/KeywordRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.jung.notify.repository;
+
+import com.jung.notify.domain.Keyword;
+import com.jung.notify.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class KeywordRepositoryTest {
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private KeywordRepository keywordRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    public void before() {
+        Member member1 = Member.builder().id("member1").build();
+        Member member2 = Member.builder().id("member2").build();
+        Member member3 = Member.builder().id("member3").build();
+        Member member4 = Member.builder().id("member4").build();
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        Keyword keyword1 = Keyword.builder()
+                .keyword("삼성전자")
+                .member(member1)
+                .build();
+        Keyword keyword2 = Keyword.builder()
+                .keyword("애플")
+                .member(member1)
+                .build();
+
+        em.persist(keyword1);
+        em.persist(keyword2);
+    }
+
+    @Test
+    public void 회원_키워드_목록조회() {
+        Member member = memberRepository.findById("member1").get();
+
+        List<Keyword> memberKeywords = keywordRepository.findAllByMember(member);
+
+        for (Keyword memberKeyword : memberKeywords) {
+            System.out.println(memberKeyword.getKeyword());
+        }
+    }
+
+    @Test
+    public void 회원_키워드_조회() {
+        Member member = memberRepository.findById("member1").get();
+
+        Optional<Keyword> keyword = keywordRepository.findOneByKeyword("삼성전자", member);
+
+        assertThat(keyword.get().getKeyword()).isEqualTo("삼성전자");
+    }
+
+    @Test
+    public void 모든_키워드_조회() {
+        List<Keyword> keywords = keywordRepository.findAll();
+
+        for (Keyword keyword : keywords) {
+            System.out.println("keyword: " + keyword.getKeyword() + " member: " + keyword.getMember().getId());
+        }
+    }
+}
