dbe7010099200138d6b07d33c84a33805516476d
yongjung95 <yongjung95@gmail.com>
2023-01-11 14:57:22 +0900

    :white_check_mark: 회원 관심 종목 메시지 전송 스케쥴러 등록

---
+++ src/main/java/com/jung/notify/schedule/StockSchedule.java
@@ -0,0 +1,23 @@
+package com.jung.notify.schedule;
+
+import com.jung.notify.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StockSchedule {
+
+    private final StockService stockService;
+
+    @Scheduled(cron = "0 1 9 1/1 * *")
+    public void morningStockScheduleV1() {
+        stockService.sendMorningStockPriceList(true);
+    }
+
+    @Scheduled(cron = "0 0 15 1/1 * *")
+    public void eveningScheduleV1() {
+        stockService.sendMorningStockPriceList(false);
+    }
+}
